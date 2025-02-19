dcd8c71aef37d116e2938db871d85bdee4ae3aeb
ababu <ababualemu123@gmail.com>
2025-02-19 19:25:22 +0300

    change project list

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,11 +1,27 @@
-// TODO Add a couple lines about each project
 const data = [
   {
-    title: "",
-    subtitle: "",
-    image: "",
-    date: "",
-    desc: " " + " " + " " + "",
+    title: "Rubix",
+    subtitle: "An admin panel for a crypto exchange platform",
+    image: "/images/projects/rubix.jpg",
+    desc: "Developed core functionalities for the Rubix admin panel, streamlining crypto transaction management. Focused on performance optimizations, security enhancements, and an intuitive dashboard for administrators.",
+  },
+  {
+    title: "Mrara Car Rental",
+    subtitle: "A car rental platform (Currently under maintenance)",
+    image: "/images/projects/mraraCarRental.jpg",
+    desc: "Contributed to the development of Mrara Car Rental, a platform designed for seamless car rentals. The project integrates secure payment processing via Skrill, ensuring safe transactions. It offers an intuitive and user-friendly experience for booking and managing car rentals online.",
+  },
+  {
+    title: "Ephone",
+    subtitle: "A mobile money platform",
+    image: "/images/projects/ephone.jpg",
+    desc: "Created a visually appealing and responsive landing page for Ephone, a mobile money platform. The page highlights the platform's key features and benefits, providing users with an engaging introduction to its mobile money, banking, and telecom services across Africa.",
+  },
+  {
+    title: "Mrara Realstate",
+    subtitle: "A realstate listing platform",
+    image: "/images/projects/mraraInvestment.jpg",
+    desc: "Developed a user-friendly platform for Mrara Investment, allowing users to browse and invest in real estate listings. The platform offers an intuitive interface for exploring available properties, viewing detailed information, and connecting with sellers for seamless transactions.",
   },
 ];
 
