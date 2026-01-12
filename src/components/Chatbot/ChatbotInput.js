d843d480e1d47f799138bb28fe221c6080e1b759
ababu <ababualemu123@gmail.com>
2026-01-12 20:39:34 +0300

    feat(chatbot): add textarea input with enter-to-send functionality

--- src/components/Chatbot/ChatbotInput.js
+++ src/components/Chatbot/ChatbotInput.js
@@ -1,17 +1,30 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const ChatbotInput = ({ input, handleInputChange, handleSend }) => {
-  return (
-    <div className="chatbot-input">
-      <input
-        type="text"
-        value={input}
-        onChange={handleInputChange}
-        placeholder="Type your message..."
-      />
-      <button onClick={handleSend}>Send</button>
-    </div>
-  );
+const ChatbotInput = ({ input, handleInputChange, handleSend }) => (
+  <div className="chatbot-input">
+    <textarea
+      rows="2"
+      value={input}
+      onChange={handleInputChange}
+      placeholder="Type your message..."
+      onKeyDown={(event) => {
+        if (event.key === "Enter") {
+          event.preventDefault();
+          handleSend();
+        }
+      }}
+    />
+    <button type="button" onClick={handleSend} className="button small">
+      Send
+    </button>
+  </div>
+);
+
+ChatbotInput.propTypes = {
+  input: PropTypes.string.isRequired,
+  handleInputChange: PropTypes.func.isRequired,
+  handleSend: PropTypes.func.isRequired,
 };
 
 export default ChatbotInput;
