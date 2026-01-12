0759f9efc2a4d54796106aa79506a868d70aefd5
ababu <ababualemu123@gmail.com>
2026-01-12 22:01:04 +0300

    feat(data): update project information and add new images

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,28 +1,43 @@
 const data = [
   {
+    title: "LexiPlan AI",
+    subtitle:
+      "Enterprise-Grade RAG Document ChatBot & Collaboration SaaS Platform",
+    image: "/images/projects/lexiplan.png",
+    desc: "Developed LexiPlan AI, a comprehensive SaaS platform that combines Retrieval-Augmented Generation (RAG) with advanced collaboration tools. The platform enables users to create AI-powered chatbots that can access and retrieve information from extensive document databases, enhancing user interactions and decision-making processes. Key features include real-time collaboration, document management, and seamless integration with various data sources to provide accurate and context-aware responses.",
+    link: "https://lexiplan-client.vercel.app/"
+  },
+  {
+    title: "CodeClue AI",
+    subtitle: "An AI-powered code explanation tool",
+    image: "/images/projects/codeclue.png",
+    desc: "Developed CodeClue AI, an innovative tool that leverages artificial intelligence to provide clear and concise explanations of code snippets. This tool assists developers in understanding complex code structures, improving code readability, and enhancing learning experiences for both novice and experienced programmers.",
+    link: "https://code-clue-client.vercel.app/"
+  },
+  {
     title: "Rubix",
     subtitle: "An admin panel for a crypto exchange platform",
     image: "/images/projects/rubix.jpg",
-    desc: "Developed core functionalities for the Rubix admin panel, streamlining crypto transaction management. Focused on performance optimizations, security enhancements, and an intuitive dashboard for administrators.",
+    desc: "Developed core functionalities for the Rubix admin panel, streamlining crypto transaction management. Focused on performance optimizations, security enhancements, and an intuitive dashboard for administrators."
   },
   {
     title: "Mrara Car Rental",
     subtitle: "A car rental platform (Currently under maintenance)",
-    image: "/images/projects/mraraCarRental.jpg",
-    desc: "Contributed to the development of Mrara Car Rental, a platform designed for seamless car rentals. The project integrates secure payment processing via Skrill, ensuring safe transactions. It offers an intuitive and user-friendly experience for booking and managing car rentals online.",
+    image: "/images/projects/mraraCarRental.png",
+    desc: "Contributed to the development of Mrara Car Rental, a platform designed for seamless car rentals. The project integrates secure payment processing via Skrill, ensuring safe transactions. It offers an intuitive and user-friendly experience for booking and managing car rentals online."
   },
   {
     title: "Ephone",
     subtitle: "A mobile money platform",
     image: "/images/projects/ephone.jpg",
-    desc: "Created a visually appealing and responsive landing page for Ephone, a mobile money platform. The page highlights the platform's key features and benefits, providing users with an engaging introduction to its mobile money, banking, and telecom services across Africa.",
+    desc: "Created a visually appealing and responsive landing page for Ephone, a mobile money platform. The page highlights the platform's key features and benefits, providing users with an engaging introduction to its mobile money, banking, and telecom services across Africa."
   },
   {
     title: "Mrara Realstate",
     subtitle: "A realstate listing platform",
     image: "/images/projects/mraraInvestment.jpg",
-    desc: "Developed a user-friendly platform for Mrara Investment, allowing users to browse and invest in real estate listings. The platform offers an intuitive interface for exploring available properties, viewing detailed information, and connecting with sellers for seamless transactions.",
-  },
+    desc: "Developed a user-friendly platform for Mrara Investment, allowing users to browse and invest in real estate listings. The platform offers an intuitive interface for exploring available properties, viewing detailed information, and connecting with sellers for seamless transactions."
+  }
 ];
 
 export default data;
